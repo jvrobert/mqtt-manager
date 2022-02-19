400dbb89b95716d7584e7f1901eb826a36a7dd2f
jvr <jvrobert@yahoo.com>
2022-02-19 00:40:24 -0700

    fix

--- include/mqtt_util.h
+++ include/mqtt_util.h
@@ -14,6 +14,12 @@ std::string JsonQuotedValue<std::string>(std::string v)
 }
 
 template <>
+std::string JsonQuotedValue<bool>(bool v)
+{
+    return v ? "true" : "false";
+}
+
+template <>
 std::string JsonQuotedValue<const char *>(const char *v)
 {
     return "\"" + std::string(v) + "\"";
